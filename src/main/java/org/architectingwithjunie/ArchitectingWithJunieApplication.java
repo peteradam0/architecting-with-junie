ade88fd0b5a3b66132af57e0b017e9ddd1ced43d
peteradam0 <adampeter1998@yahoo.com>
2026-02-05 14:46:32 +0200

    feat(setup): initial commit

---
+++ src/main/java/org/architectingwithjunie/ArchitectingWithJunieApplication.java
@@ -0,0 +1,13 @@
+package org.architectingwithjunie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectingWithJunieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchitectingWithJunieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/architectingwithjunie/ArchitectingWithJunieApplicationTests.java
@@ -0,0 +1,13 @@
+package org.architectingwithjunie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectingWithJunieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
